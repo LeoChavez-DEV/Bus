2dc33822e733062fb4f3036f5f6d619ff2fdf2a7
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-09-19 19:42:26 +0200

    arrelgando bus

--- bus.py
+++ bus.py
@@ -1,39 +1,29 @@
-class bus:
-    def Bus():
-        def __init__(self, idBus, bus, plazasLibres, plazasComletas):
-            self.setIdBus(idBus)
-            self.setBus(bus)
-            self.setPlazasLibres(plazasLibres)
-            self.setPlazasComletas(plazasComletas)
+class Bus:
+    
+    def __init__(self, idBus, bus, capacidad):
+        self.setIdBus(idBus)
+        self.setBus(bus)
+        self.setCapacidad(capacidad)
 
-        # Getters      
-        def getIdBus(self):
-            return self.__idBus
+    # Getters      
+    def getIdBus(self):
+        return self.__idBus
 
-        def getBus(self):
-            return self.__bus
+    def getBus(self):
+        return self.__bus
 
-        def getPlazasLibres(self):
-            return self.__plazasLibres
+    def getCapacidad(self):
+        return self.__capacidad
 
-        def getPlazasComletas(self):
-            return self.__plazasComletas
+    # Setters
+    def setIdBus(self, idBus):
+        self.__idBus = idBus
+    
+    def setBus(self, bus):
+        self.__bus = bus
+    
+    def setCapacidad(self, capacidad):
+        self.__capacidad = capacidad
 
-        # Setters
-        def setIdBus(self, idBus):
-            self.__idBus = idBus
-        
-        def setBus(self, bus):
-            self.__bus = bus
-        
-        def setPlazasCompletas(self, plazasComletas):
-            self.__plazasCompletas = plazasComletas
-        
-        def setPlazasLibres(self, plazasLibres):
-            self.__plazasLibres = plazasLibres
-
-        def __str__(self):
-            return f"ID: {self.getIdBus()}, Buses: {self.getBus()}, plazaCompletas: {self.getPlazasCompletas()}, plazasLibres: {self.getPlazasLibres()}"
-        
-        
-
+    def __str__(self):
+        return f"ID: {self.getIdBus()}, Buses: {self.getBus()}, capacidad: {self.getCapacidad()}"
