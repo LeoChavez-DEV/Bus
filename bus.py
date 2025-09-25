1466dbc55c80196df10a455258c4aaf2302a352c
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-09-25 16:46:57 +0200

    avances en el bus

--- bus.py
+++ bus.py
@@ -25,6 +25,30 @@ class Bus:
     def capacidadLibre(self):
         return self.__capacidad - len(self._billetes)
     
-
+    def venderBillete(self, cliente):
+        if self.capacidadLibre() <= 0:
+            print ("No hay plazas disponibles")
+        
+        billete = Billete(cliente, self)
+        self.billetes.append(billete)
+        return billete
+    
+    def devolverBillete(self, cliente):
+        for b in self._billetes:
+            if b.cliente == cliente:
+                self._billetes.remove(b)
+                return True
+        return False
+    
+    def estado(self):
+        return (
+        f"Bus {self._nombre} (ID {self._id_bus})\n"
+        f"Total: {self._capacidad}\n"
+        f"Libre: {self.capacidad_libre()}\n"
+        f"Vendido: {len(self._billetes)}"
+    )
+        
     def __str__(self):
-        return f"ID: {self.getIdBus()}, Capacidad: {self.getCapacidad()}"
+        return f"ID: {self._id_bus}, Capacidad: {self._capacidad}"
+    
+    # 
