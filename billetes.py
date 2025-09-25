c20c2c843ccb08cfae94d469166aeca1a74c6b53
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-09-25 16:17:29 +0200

    solucionando error de imports

--- billetes.py
+++ billetes.py
@@ -1,13 +1,12 @@
 from cliente import Cliente
-from bus import Bus
 
 class Billete:
-    def __init__(self, cliente: Cliente, bus: Bus):
-        self.setCliente(cliente)
-        self.setBus(bus)
+    def __init__(self, cliente, bus):
+        self.cliente = cliente
+        self.bus = bus
 
     def vender(self):
-        return self.bus.vender_billete(self.cliente)
-    
+        return self.bus.venderBillete(self.cliente)
+
     def devolver(self):
-        return self.bus.devolver_billete(self.cliente)
+        return self.bus.devolverBillete(self.cliente)
