b152a89dc73b42968b29011d5b841013e2e749e7
Àlex Milian Dieguez <alexmiliandieguez@MacBook-Pro-de-Alex-3.local>
2025-09-25 16:20:31 +0200

    Main refresh

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from cliente import Cliente
 from bus import Bus
 from billetes import Billete
 
-menu = "\n1.- Venta de billetes\n2.- Devolución de billetes\n3.- Estado de la venta\n0.- Salir"
+menu = "\n1. - Crear bus\n2.- Venta de billetes\n3.- Devolución de billetes\n4.- Estado de la venta\n0.- Salir"
+
+buses = []
 
 while True:
     bus_id = str(input("ID bus: "))
@@ -14,20 +16,27 @@ while True:
     opcion = input("Opción: ")
 
     if opcion == "1":
+        bus_id = input("Bus ID: ")
+        capacidad = int(input("Número plazas: "))
+        bus = Bus(bus_id, capacidad)
+        buses.append(bus)
+        print(f"Bus {bus_id} con capacidad {capacidad}.")
+    
+    elif opcion == "2":
         nombre = input("Nombre del cliente: ")
         apellido = input("Apellido del cliente: ")
         cliente = Cliente(nombre, apellido)
         billete = Billete(cliente, bus)
         print(billete.vender())
 
-    elif opcion == "2":
+    elif opcion == "3":
         nombre = input("Nombre del cliente: ")
         apellido = input("Apellido del cliente: ")
         cliente = Cliente(nombre, apellido)
         billete = Billete(cliente, bus)
         print(billete.devolver())
 
-    elif opcion == "3":
+    elif opcion == "4":
         print(bus.estado_bus())
 
     elif opcion == "0":
