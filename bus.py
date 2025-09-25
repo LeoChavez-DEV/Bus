287c636eae8628b2577e137805028eee490c0071
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-09-25 16:08:49 +0200

    avance en bus

--- bus.py
+++ bus.py
@@ -1,16 +1,16 @@
+from cliente import Cliente
+from billetes import Billete
+
 class Bus:
     
-    def __init__(self, idBus, bus, capacidad):
+    def __init__(self, idBus, capacidad):
         self.setIdBus(idBus)
-        self.setBus(bus)
         self.setCapacidad(capacidad)
+        self._billetes = []
 
     # Getters      
     def getIdBus(self):
         return self.__idBus
-
-    def getBus(self):
-        return self.__bus
 
     def getCapacidad(self):
         return self.__capacidad
@@ -19,11 +19,12 @@ class Bus:
     def setIdBus(self, idBus):
         self.__idBus = idBus
     
-    def setBus(self, bus):
-        self.__bus = bus
-    
     def setCapacidad(self, capacidad):
         self.__capacidad = capacidad
+        
+    def capacidadLibre(self):
+        return self.__capacidad - len(self._billetes)
+    
 
     def __str__(self):
-        return f"ID: {self.getIdBus()}, Buses: {self.getBus()}, capacidad: {self.getCapacidad()}"
+        return f"ID: {self.getIdBus()}, Capacidad: {self.getCapacidad()}"
